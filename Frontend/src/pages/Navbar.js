22b75b5c961a5bb97d7899c64783b878154c7598
jaypatel342005 <pateljay97378@gmail.com>
2024-10-25 18:00:12 +0530

    MERN

--- Frontend/src/pages/Navbar.js
+++ Frontend/src/pages/Navbar.js
@@ -2,8 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css"; // Import custom CSS styles
 import appleLogo from './icons8-apple.svg'; // Import the local image
+import { useState } from 'react';
+
 
 const Navbar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const handleToggle = () => {
+    setCollapsed(!collapsed);
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-black">
       <a className="navbar-brand mx-" href="/home">
@@ -17,16 +23,17 @@ const Navbar = () => {
         
       </a>
       <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      onClick={handleToggle}  // Trigger state change onClick
+      className={`navbar-toggler ${collapsed ? 'collapsed' : ''}`} // Dynamically add 'collapsed' class
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded={!collapsed}  // Set aria-expanded based on the collapsed state
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
       <div
         className="collapse navbar-collapse justify-content-center"
         id="navbarNav"
